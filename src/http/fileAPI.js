a58f2392e207d6fe439b874a8fd4ec6602e13e8d
defk1lla <set.adoyan@gmail.com>
2022-07-05 20:39:39 +0400

    file upload test

--- src/http/fileAPI.js
+++ src/http/fileAPI.js
@@ -1,7 +1,8 @@
 import { $authHost } from "./index";
 
 export const upload = async (formData) => {
+   console.log(1)
    const response = await $authHost.post('file', formData);
-
+   console.log(response)
    return response.data;
 };
